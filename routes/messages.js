beffcf24e339341c6121a49f5c62493abe49a749
ardhln <92050709+ardhln@users.noreply.github.com>
2023-10-10 13:00:24 +0700

    Update messages.js

--- routes/messages.js
+++ routes/messages.js
@@ -1,15 +1,12 @@
 const { addMessage, getMessages } = require("../controllers/messageController");
 const router = require("express").Router();
-const multer = require("multer"); // Import multer
+const multer = require("multer");
 
-// Konfigurasi multer
-const storage = multer.memoryStorage(); // Simpan file dalam memory
+
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Rute untuk menambahkan pesan dengan menggunakan multer
 router.post("/addmsg", upload.single("file"), addMessage);
-
-// Rute untuk mendapatkan pesan
 router.get("/getmsg", getMessages);
 
 module.exports = router;
